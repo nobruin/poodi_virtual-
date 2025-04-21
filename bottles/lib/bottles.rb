07d1da120cf29fc62543239f1a34a507b5fd731b
nobruin <nobruin@gmail.com>
2025-04-21 10:56:18 -0300

    feat: first exercise

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,27 @@
 class Bottles
+    def song()
+        verses(99, 0)
+    end
+    
+    def verses(start, finish)
+        start.downto(finish).map do |n|
+            verse(n)
+        end.join("\n")
+    end
 
+    def verse(n)
+        if n == 0
+          "No more bottles of beer on the wall, no more bottles of beer.\n" \
+          "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+        elsif n == 1
+          "1 bottle of beer on the wall, 1 bottle of beer.\n" \
+          "Take it down and pass it around, no more bottles of beer on the wall.\n"
+        elsif n == 2
+          "2 bottles of beer on the wall, 2 bottles of beer.\n" \
+          "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+        else
+          "#{n} bottles of beer on the wall, #{n} bottles of beer.\n" \
+          "Take one down and pass it around, #{n - 1} bottles of beer on the wall.\n"
+        end
+    end      
 end
